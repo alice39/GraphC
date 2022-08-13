4a9bb08f07861f531a090632e1a3407c3813193b
A.L.I.C.E <20363359+alice39@users.noreply.github.com>
2022-08-12 23:19:55 -0400

    vertex: add capacity to struct vertex_array

--- src/vertex.c
+++ src/vertex.c
@@ -9,20 +9,21 @@ void vertex_array_from(struct vertex_array* array, const vertex_t* data, size_t 
     }
 
     // reserva el espacio suficiente si es necesario
-    if (len > array->len) {
-        vertex_array_reserve(array, len - array->len);
+    if (len > array->capacity) {
+        vertex_array_reserve(array, len - array->capacity);
     }
 
+    array->len = len;
     memcpy(array->data, data, sizeof(vertex_t) * len);
 }
 
 void vertex_array_reserve(struct vertex_array* array, size_t capacity) {
-    if (array == NULL || capacity == 0) {
+    if (array == NULL || array->capacity > array->len + capacity) {
         return;
     }
 
-    size_t new_len = array->len + capacity;
-    vertex_t* new_data = realloc(array->data, sizeof(vertex_t) * new_len);
+    size_t new_cap = array->capacity + capacity;
+    vertex_t* new_data = realloc(array->data, sizeof(vertex_t) * new_cap);
 
     // es posible que la nueva capacidad no pueda
     // ser reservada por falta de memoria
@@ -30,7 +31,7 @@ void vertex_array_reserve(struct vertex_array* array, size_t capacity) {
         return;
     }
 
-    array->len = new_len;
+    array->capacity = new_cap;
     array->data = new_data;
 }
 
@@ -41,6 +42,8 @@ void vertex_array_clone(const struct vertex_array* array, struct vertex_array* o
 
     vertex_array_destroy(out);
     vertex_array_reserve(out, array->len);
+
+    out->len = array->len;
     memcpy(out->data, array->data, sizeof(vertex_t) * array->len);
 }
 
@@ -63,6 +66,7 @@ void vertex_array_destroy(struct vertex_array* array) {
         return;
     }
 
+    array->capacity = 0;
     array->len = 0;
     free(array->data);
     array->data = NULL;
